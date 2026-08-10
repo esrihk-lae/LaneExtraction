509717183f5a5440b43c9e80931eb067463772eb
dky.esri <dky.esri@outlook.com>
2026-08-10 15:40:47 +0800

    re-work for a slightly more modern stack and workflow; replaced scipy.ndimage.imread with imageio.v2.imread

---
+++ code/laneAndDirectionExtraction/run_inference.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# Derek Yuen
+# 2026-08
+
+# previously known as infer_pipeline.py in the Unofficial version
+# rewritten in bash
+
+
+#for i in 0 5 6 11 12 17 18 22 25 28 31; do
+for i in 0 9 11 12 17; do
+
+    inputfile="../../dataset/sat_${i}.jpg"
+    outputfolder="./output/${i}"
+    model="resnet34v3"
+    
+    echo "${inputfile} -> ${outputfolder}, model: ${model}"
+
+    # rm -f "${outputfolder}"/*
+    mkdir -vp "${outputfolder}"
+
+    # copy or replace the sdmap file into the expected filename format; overwrite if necessary
+    cp -fv "validation_laneExtraction_run1_640_resnet34v3_500ep/sdmap${i}.jpg" "../../dataset/sdmap_${i}.jpg"
+
+    echo "run inference for the first stage and extract the graph"
+    python3 infer.py "${inputfile}" "${outputfolder}" "${model}"
+    echo
+
+    echo "Turning segmentation to graph"
+    python3 segtograph/setograph.py "${outputfolder}/seg.png" 64 "${outputfolder}/graph.p"
+    echo
+
+    # extract directions and create ways.json file
+    python3 infer_direction.py "${outputfolder}/direction.png" "${outputfolder}/graph.p" "${outputfolder}"
+    echo
+
+    rm -fv "../../dataset/sdmap_${i}.jpg"
+done
---
+++ code/laneAndDirectionExtraction/run_training.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Derek Yuen
+# 2026-08
+
+#CUDA_VISIBLE_DEVICES=1 
+
+python3 ./train.py resnet34v3
