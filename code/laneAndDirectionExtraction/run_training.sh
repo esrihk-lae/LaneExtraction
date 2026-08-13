7571e35a2605b9d171dd529eab00361450be2489
dky.esri <dky.esri@outlook.com>
2026-08-13 12:25:25 +0800

    enable TF_ENABLE_AUTO_MIXED_PRECISION

--- code/laneAndDirectionExtraction/run_training.sh
+++ code/laneAndDirectionExtraction/run_training.sh
@@ -2,6 +2,8 @@
 # Derek Yuen
 # 2026-08
 
-#CUDA_VISIBLE_DEVICES=1 
+#CUDA_VISIBLE_DEVICES=1
+#export TF_ENABLE_AUTO_MIXED_PRECISION=1
+#TF_ENABLE_AUTO_MIXED_PRECISION=1 python3 ./train.py resnet34v3
 
 python3 ./train.py resnet34v3
