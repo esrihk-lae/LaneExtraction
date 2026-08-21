6623afc07d3690dc8fd109bcb07cb4db2aceefd4
dky.esri <dky.esri@outlook.com>
2026-08-21 16:18:38 +0800

    runtime fixes

--- code/turningLaneExtraction/run_inference.sh
+++ code/turningLaneExtraction/run_inference.sh
@@ -2,7 +2,9 @@
 # Derek Yuen
 # 2026-08
 
-# previously known as infer_pipeline.py in the Unofficial version
+# previously known as run_test.bash / infer_pipeline.py in the Unofficial version
+# https://github.com/TonyXuQAQ/UnofficialLaneExtraction/blob/master/code/turningLaneExtraction/run_test.bash
+# https://github.com/TonyXuQAQ/UnofficialLaneExtraction/blob/master/code/turningLaneExtraction/infer_pipeline.py
 # rewritten in bash
 
 
@@ -21,8 +23,8 @@ for i in 0 5 6 11 12 17 18 22 25 28 31; do
     cp -fv "validation_laneExtraction_run1_640_resnet34v3_500ep/sdmap${i}.jpg" "../../dataset/sdmap_${i}.jpg"
 
     echo "run inference for the first stage and extract the graph"
-    python3 infer.py "${inputfile}" "${outputfolder}" "${model}" ; echo ""
-
+    #python3 infer.py "${inputfile}" "${outputfolder}" "${model}" ; echo ""
+    python infer_link_v4.py "$inputfile" "$outputfolder/direction.png" "$outputfolder/graph.p" "$outputfolder"
 
     echo "Turning segmentation to graph"
     python3 segtograph/setograph.py "${outputfolder}/seg.png" 64 "${outputfolder}/graph.p" ; echo ""
