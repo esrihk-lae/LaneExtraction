979d61242fd4fbd499816c75bedb3f2023b5a66d
dky.esri <dky.esri@outlook.com>
2026-08-25 18:18:33 +0800

    initial commit

---
+++ code/turningLaneValidation/run_infer_link.py
@@ -0,0 +1,13 @@
+from subprocess import Popen 
+import sys 
+
+#for i in [0,5,6,11,12,17,18,22,25,28,31]:
+for i in [5]:
+    inputfile = '../../dataset/sat_%s.jpg'%i
+    outputfolder = '../laneAndDirectionExtraction/output/%s'%i
+    model = 'resnet34v3'
+    # Popen("rm %s/*" % outputfolder, shell=True).wait()
+
+    # extract turning lanes
+    # python3 ./infer_link_v4.py ../../dataset/sat_5.jpg ../laneAndDirectionExtraction/output/5/direction.png ../laneAndDirectionExtraction/output/5/graph.p  ../laneAndDirectionExtraction/output/5 output
+    Popen("python infer_link_v4.py %s %s %s %s" % (inputfile, outputfolder + "/direction.png", outputfolder + "/graph.p", outputfolder), shell=True).wait()
