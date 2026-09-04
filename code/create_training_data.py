47532c18f858d1ed662cb79f182774f256d54659
dky.esri <dky.esri@outlook.com>
2026-09-04 18:44:13 +0800

    replace old popen with os.makedirs()

--- code/create_training_data.py
+++ code/create_training_data.py
@@ -1,8 +1,28 @@
 #import sys
-from subprocess import Popen
+#from subprocess import Popen
+import subprocess
+import os
 
-Popen("mkdir -p dataset_training", shell=True).wait()
+dataset_dir = '../dataset'
+regions_json = f"{dataset_dir}/regions.json"     # strings only =(  )
+
+training_data_dir = "dataset_training"
+eval_data_dir = "dataset_evaluation"
+
+#Popen("mkdir -p dataset_training", shell=True).wait()
+os.makedirs(training_data_dir, exist_ok=True)
+
 #Popen("mkdir dataset_evaluation", shell=True).wait()
+os.makedirs(eval_data_dir, exist_ok=True)
 
-Popen("python3 hdmapeditor/create_dataset_for_training.py ../dataset/regions.json ../dataset/ dataset_training/", shell=True).wait()
-Popen("python3 hdmapeditor/create_dataset_for_training_vectors.py ../dataset/regions.json ../dataset/ dataset_training/", shell=True).wait()
+
+#args = [regions_json, dataset_dir, training_data_dir]
+
+#Popen("python3 hdmapeditor/create_dataset_for_training.py ../dataset/regions.json ../dataset/ dataset_training/", shell=True).wait()
+r = subprocess.run(["python3", "hdmapeditor/create_dataset_for_training.py", regions_json, dataset_dir, training_data_dir], capture_output=True, text=True)
+
+print(f"stdout: {r.stdout}")
+print(f"stderr: {r.stderr}")
+
+#Popen("python3 hdmapeditor/create_dataset_for_training_vectors.py ../dataset/regions.json ../dataset/ dataset_training/", shell=True).wait()
+
